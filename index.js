13e0d2051e945d25396a08357757bc27205b1345
Edgaras Narbutas <edgar.narbutas@gmail.com>
2020-05-15 00:57:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,5 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if(string.toLowerCase){return"I can't hear you!"}
-}
-function sayHiToGrandma(string) {
-  if(string.toUpperCase){return "YES INDEED!"}
+else(string.toUpperCase){return "YES INDEED!"}
 }
