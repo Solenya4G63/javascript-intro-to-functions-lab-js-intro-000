85e82eb04d41ee48dea126b8244c7c85b0eb4b6b
Edgaras Narbutas <edgar.narbutas@gmail.com>
2020-05-15 01:29:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string==lowercase.toLowerCase()){return"I can't hear you!"}
-  else (string==.toUpperCase()){return"YES INDEED!"}
-  else (string == "I love you, Grandma."){return"I love you, too."}
+  if (string.lowercase.toLowerCase()){return"I can't hear you!"}
+  else (string.toUpperCase()){return"YES INDEED!"}
+  else ("I love you, Grandma."){return"I love you, too."}
 }
